3d9393ea6596d4780f7d81c5e9ee33c0ca524bc9
ninjawarrior1337 <t.rothled@gmail.com>
2022-09-22 13:00:44 -0700

    realtime comms are back

---
+++ src/routes/apple/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+import Pusher from "pusher-js"
+import {PUBLIC_PUSHER_APP_KEY} from "$env/static/public"
+
+var pusher = new Pusher(PUBLIC_PUSHER_APP_KEY, {
+    cluster: "us3"
+})
+var channel = pusher.subscribe("c")
+channel.bind("e", (d) => {
+    console.log(d)
+})
+</script>
+
+<div>
+    {PUBLIC_PUSHER_APP_KEY}
+</div>
