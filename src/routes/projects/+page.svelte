fa407f34e0a68dc0fc54bb0f4faa958512b23862
ninjawarrior1337 <me@treelar.xyz>
2024-05-18 23:22:26 -0700

    i have no idea why that was there

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,5 +1,5 @@
 <template>
-    <div class="bg-gray-900 min-h-screen w-full py-8 flex flex-col">
+    <div class="min-h-screen w-full py-8 flex flex-col">
         <h1 class="text-center font-bold text-5xl py-3 px-8 mb-12 bg-gray-700 rounded-full self-center">Projects</h1>
         <div class="grid gap-y-16 lg:grid-cols-2 items-center justify-items-center">
             {#each projects as project}
