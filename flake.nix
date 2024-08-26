e290872d3b93d3f3d255c194d370d18d2048104f
ninjawarrior1337 <me@treelar.xyz>
2024-08-26 15:37:40 -0700

    switch to using nix instead of devcontainers

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          nodejs_20
+          pnpm
+        ];
+      };
+    });
+}
